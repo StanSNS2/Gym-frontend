2539d96b1825875ba06acbc7605f6d460d52a671
Stanimir Sergev <stanimir.sergev@dataparkinc.com>
2024-03-20 12:21:41 +0200

    add Header section.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import './App.css';
-import axios from "axios";
-import {BACKEND_BASE_URL} from "./Constants/globalConst";
+import Header from "./Component/STATIC/Header";
+import {BrowserRouter} from 'react-router-dom'
+
 
 function App() {
-    const getData = () => {
-        console.log(BACKEND_BASE_URL)
-        axios.get(BACKEND_BASE_URL + "/getAllUsers")
-            .then((res) => {
-                console.log(res)
-            }).catch((err) => {
-            console.log(err)
-        })
-
-    }
 
     return (
-        <div>s
-            <button onClick={getData}>
-                Click me test
-            </button>
-        </div>
+        <>
+            <BrowserRouter>
+                <Header/>
+
+
+            </BrowserRouter>
+
+        </>
     );
 }
 
