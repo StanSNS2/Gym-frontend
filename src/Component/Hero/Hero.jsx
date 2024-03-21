5c1a98d9080aedfd6ac4a966e377672bc256fcd0
Stanimir Sergev <stanimir.sergev@dataparkinc.com>
2024-03-21 10:19:08 +0200

    add static images to hero and shop sections.

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import "./Hero.css";
+import img1 from "../../Resources/Hero/img1.jpg"
+import img4 from "../../Resources/Hero/img4.jpg"
+import img20 from "../../Resources/Hero/img20.jpg"
+import img5 from "../../Resources/Hero/img5.jpg"
+import img26 from "../../Resources/Hero/img26.jpg"
+import img16 from "../../Resources/Hero/img16.jpg"
+import img22 from "../../Resources/Hero/img22.jpg"
+import img29 from "../../Resources/Hero/img29.jpg"
+import img21 from "../../Resources/Hero/img21.jpg"
+import img19 from "../../Resources/Hero/img19.jpg"
+import img2 from "../../Resources/Hero/img2.jpg"
+import img28 from "../../Resources/Hero/img28.jpg"
+import img13 from "../../Resources/Hero/img13.jpg"
+import img8 from "../../Resources/Hero/img8.jpg"
+import img14 from "../../Resources/Hero/img14.jpg"
+import img3 from "../../Resources/Hero/img3.jpg"
+import img18 from "../../Resources/Hero/img18.jpg"
+import img17 from "../../Resources/Hero/img17.jpg"
+import img10 from "../../Resources/Hero/img10.jpg"
+import img27 from "../../Resources/Hero/img27.jpg"
+import img9 from "../../Resources/Hero/img9.jpg"
+import img12 from "../../Resources/Hero/img12.jpg"
+import img24 from "../../Resources/Hero/img24.jpg"
+import img6 from "../../Resources/Hero/img6.jpg"
 
 function Hero() {
     const carousel1Images = [
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+        img1,
+        img4,
+        img20,
+        img5,
+        img26,
+        img16,
+        img22,
+        img29,
+        img21,
+        img19,
+        img28,
+        img13,
+        img10,
+
     ];
 
     const carousel2Images = [
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+        img18,
+        img17,
+        img2,
+        img27,
+        img9,
+        img12,
+        img24,
+        img6,
+        img8,
+        img14,
+        img3
     ];
 
     return (
--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -4,139 +4,161 @@ import "./Shop.css"
 import {FaShoppingCart} from "react-icons/fa";
 import {CgDollar} from "react-icons/cg";
 
+import img1 from "../../Resources/Shop/img1.png"
+import img2 from "../../Resources/Shop/img2.jpg"
+import img3 from "../../Resources/Shop/img3.jpg"
+import img4 from "../../Resources/Shop/img4.jpg"
+import img5 from "../../Resources/Shop/img5.jpg"
+import img6 from "../../Resources/Shop/img6.jpg"
+import img7 from "../../Resources/Shop/img7.jpg"
+import img8 from "../../Resources/Shop/img8.jpg"
+import img9 from "../../Resources/Shop/img9.png"
+import img10 from "../../Resources/Shop/img10.jpg"
+import img11 from "../../Resources/Shop/img11.jpg"
+import img12 from "../../Resources/Shop/img12.jpg"
+import img13 from "../../Resources/Shop/img13.jpg"
+import img14 from "../../Resources/Shop/img14.jpg"
+import img15 from "../../Resources/Shop/img15.jpg"
+import img16 from "../../Resources/Shop/img16.jpg"
+import img17 from "../../Resources/Shop/img17.jpg"
+import img18 from "../../Resources/Shop/img18.jpg"
+import img19 from "../../Resources/Shop/img19.jpg"
+
 const Shop = () => {
     const products = [
         {
             id: 1,
-            name: "Product 1",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-            price: 255.58,
+            name: "Whey Protein",
+            description: "Premium whey protein powder for muscle building and recovery. Made with high-quality ingredients and available in various flavors.",
+            imageUrl: img1,
+            price: 39.99,
         },
         {
             id: 2,
-            name: "Product 2",
-            description: "Description of Product 2",
-            imageUrl: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            price: 25,
+            name: "Creatine Monohydrate",
+            description: "Pure micronized creatine monohydrate for increased strength and power. Enhance your performance during high-intensity workouts.",
+            imageUrl: img2,
+            price: 19.99,
         },
         {
             id: 3,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-            price: 25,
+            name: "BCAA",
+            description: "Branch chain amino acids supplement for muscle preservation and endurance. Supports muscle recovery and reduces muscle fatigue.",
+            imageUrl: img3,
+            price: 29.99,
         },
         {
             id: 4,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            price: 25,
+            name: "Pre-Workout",
+            description: "Energy-boosting pre-workout supplement with caffeine and beta-alanine. Get pumped up and improve focus during your workouts.",
+            imageUrl: img4,
+            price: 34.99,
         },
         {
             id: 5,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Fish Oil",
+            description: "Omega-3 fish oil supplement for joint health and inflammation reduction. Supports heart health and cognitive function.",
+            imageUrl: img5,
+            price: 14.99,
         },
         {
             id: 6,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            price: 25,
+            name: "Multivitamin",
+            description: "Comprehensive multivitamin for overall health and well-being. Provides essential vitamins and minerals for daily nutritional support.",
+            imageUrl: img6,
+            price: 24.99,
         },
         {
             id: 7,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Glutamine",
+            description: "Pure L-glutamine powder for muscle recovery and immune support. Helps reduce muscle soreness and improve post-workout recovery.",
+            imageUrl: img7,
+            price: 22.99,
         },
         {
             id: 8,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            price: 25,
+            name: "Casein Protein",
+            description: "Slow-digesting casein protein powder for sustained muscle protein synthesis. Ideal for consumption before bedtime.",
+            imageUrl: img8,
+            price: 49.99,
         },
         {
             id: 9,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Fat Burner",
+            description: "Thermogenic fat burner to support weight loss and increase metabolism. Formulated with natural ingredients.",
+            imageUrl: img9,
+            price: 29.99,
         },
         {
             id: 10,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Protein Bar",
+            description: "Convenient protein bar for on-the-go nutrition. Packed with protein and low in sugar for guilt-free snacking.",
+            imageUrl: img10,
+            price: 2.99,
         },
         {
             id: 11,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            price: 25,
+            name: "Whey Protein",
+            description: "Premium whey protein powder for muscle building and recovery.",
+            imageUrl: img11,
+            price: 39.99,
         },
         {
             id: 12,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 255.58,
+            name: "Creatine Monohydrate",
+            description: "Pure micronized creatine monohydrate for increased strength and power.",
+            imageUrl: img12,
+            price: 19.99,
         },
         {
             id: 13,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "BCAA",
+            description: "Branch chain amino acids supplement for muscle preservation and endurance.",
+            imageUrl: img13,
+            price: 29.99,
         },
         {
             id: 14,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            price: 25,
+            name: "Pre-Workout",
+            description: "Energy-boosting pre-workout supplement with caffeine and beta-alanine.",
+            imageUrl: img14,
+            price: 34.99,
         },
         {
             id: 15,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Fish Oil",
+            description: "Omega-3 fish oil supplement for joint health and inflammation reduction.",
+            imageUrl: img15,
+            price: 14.99,
         },
         {
             id: 16,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Glutamine",
+            description: "Pure L-glutamine powder for muscle recovery and immune support.",
+            imageUrl: img16,
+            price: 24.99,
         },
         {
             id: 17,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s=",
-            price: 25,
+            name: "Casein Protein",
+            description: "Slow-release casein protein powder for sustained muscle repair and growth.",
+            imageUrl: img17,
+            price: 49.99,
         },
         {
             id: 18,
-            name: "Product 3",
-            description: "Description of Product 3",
-            imageUrl: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg"
+            name: "Citrulline Malate",
+            description: "Nitric oxide booster for enhanced blood flow and muscle pumps during workouts.",
+            imageUrl: img18,
+            price: 27.99,
         },
         {
             id: 19,
-            name: "Product  sasdsa dsad asd asd asd ds ad sad a asd asd ds daasd 3",
-            description: "Description of Prod sas dsd sd s sa as as sad sadasdas sadsad sad sadsas uct 3",
-            imageUrl: "https://media.istockphoto.com/id/1322220448/photo/abstract-digital-futuristic-eye.jpg?s=612x612&w=0&k=20&c=oAMmGJxyTTNW0XcttULhkp5IxfW9ZTaoVdVwI2KwK5s="
-        }
+            name: "ZMA",
+            description: "Zinc, magnesium, and vitamin B6 complex for better sleep and recovery.",
+            imageUrl: img19,
+            price: 21.99,
+        },
     ];
 
     const [currentPage, setCurrentPage] = useState(1);
